dc9db633a6cfd6bc4595d900c209c72d01d7da85
ssingh-calance <ssingh@calance.com>
2026-05-07 17:41:37 +0530

    Update ESLint config to ignore specific snapshot file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,4 +2,23 @@ import js from '@eslint/js';
 
 export default [
   js.configs.recommended,
+  {
+    ignores: [
+      'lib/connection/snapshots/quotes/retrieveSnapshots.js',
+    ],
+    languageOptions: {
+      globals: {
+        require: 'readonly',
+        module: 'readonly',
+        exports: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        process: 'readonly',
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'script',
+      },
+    },
+  },
 ];
